ee2dfc07edf7a7c33d57348e2abac8eb963031e9
Humberto <humberto.filho@benie.com.br>
2021-07-08 20:49:48 -0400

    feat(*): Create routes and export files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from './routes';
+import theme from './assets/styles/themes/theme';
+import GlobalStyle from './assets/styles/global';
+
+const App: React.FC<unknown> = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Routes />
+            <ToastContainer autoClose={4000} />
+            <GlobalStyle />
+        </ThemeProvider>
+    );
+};
 
 export default App;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <Col className="d-flex align-items-center justify-content-center h-100">
+            <h1 className="font-weight-bold">Hello world, CAECOMP!</h1>
+        </Col>
+    );
+}
+
+export default Home;
