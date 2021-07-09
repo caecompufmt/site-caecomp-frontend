91e52371e9347c467d04ac1fe9d07158d0441d57
Humberto <humberto.filho@benie.com.br>
2021-07-08 20:48:40 -0400

    chore(styles): Create global styles

---
+++ src/assets/styles/themes/theme.ts
@@ -0,0 +1,44 @@
+export default {
+    title: 'theme',
+
+    colors: {
+        primaryBlue: '#00A9E0',
+        secondaryBlue: '#007AC9',
+        secondaryYellow: '#F2AF00',
+        secondaryPurple: '#753E91',
+
+        white: '#FFF',
+        dark: '#1D1D1B',
+        light: '#A5ACAF',
+
+        success: '#008620',
+        error: '#DC3545',
+        warning: '#F2AF00',
+        info: '#00A9E0',
+
+        textDark: '#1D1D1B',
+        textLight: '#A5ACAF',
+
+        borderColor: '#EDEDED',
+        backgroundColor: '#F9F9F9',
+        hoverLight: 'rgba(183, 182, 182, 0.2)',
+    },
+    borderRadius: {
+        default: '16px',
+        small: '12px',
+        xsmall: '6px',
+    },
+    queries: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1400px',
+    },
+    boxShadow: {
+        highest: '0 8px 24px rgba(0, 0, 0, .25);',
+        high: '0 6px 16px rgba(0, 0, 0, .25);',
+        medium: '0 8px 8px rgba(0, 0, 0, .25);',
+        low: '0 2px 4px rgba(0, 0, 0, .25);',
+    },
+};
